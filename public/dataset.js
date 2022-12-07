f27eb1ea2e732f85d9822ee045f0fcc8a813d3b8
Carl Antiado <carl.antiado01@gmail.com>
2022-12-06 20:25:02 -0800

    Changed option text to settings gear icon

--- public/dataset.js
+++ public/dataset.js
@@ -1,3 +1,9 @@
 const searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", () => {
+  console.log("Search button clicked!");
+});
 
-searchButton.addEventListener("click", () => {console.log("Search button clicked!")})
+const optionsButton = document.querySelector("#options-button");
+optionsButton.addEventListener("click", () => {
+  console.log("Options button clicked!");
+});

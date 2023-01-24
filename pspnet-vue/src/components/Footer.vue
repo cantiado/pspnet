0607a4b37fa8b165aaab980c962ef0cd706bc3c6
antoniolang1107 <TonylsLame@users.noreply.github.com>
2023-01-24 11:47:25 -0800

    Created basic footer component

---
+++ pspnet-vue/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<!-- Author: Antonio Lang -->
+
+<template>
+    <footer class="bg-gray-200 text-center lg:text-left"> <!-- from Tailwind -->
+        <div class="text-gray-700 text-center p-4" style="background-color: #663300ff; color: white;">
+            © 2023 Copyright: PSPNet
+        </div>
+    </footer>
+</template>
+
+<style>
+    .footer {
+        height: 50px;
+        bottom: 0px;
+    }
+</style>

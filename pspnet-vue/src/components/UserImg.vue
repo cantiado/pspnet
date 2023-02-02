7f396c367a003a23315a0949a2ffce0f2e973557
antoniolang1107 <TonylsLame@users.noreply.github.com>
2023-02-02 01:21:31 -0800

    User profile is more flexible Data being displayed are now refereced from data rather than hard-coded (though not yet read from file) User images to be displayed can be shown by passing the name of the image (UserImg Vue component)

---
+++ pspnet-vue/src/components/UserImg.vue
@@ -0,0 +1,29 @@
+<!-- Author: Antonio Lang -->
+
+<template>
+    <div class="flex flex-wrap justify-center">
+        <img class="object-cover h-48 w-48 p-1 bg-white border rounded max-w-sm" :src="require(`../assets/${src_path}`)">
+    </div>
+    <div class="mdc-image-list__supporting">
+        <span class="mdc-image-list__label">"Un/verified"</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['src_path'],
+    setup() {
+        let name = 'UserImg'
+        return {name}
+    },
+    data() {
+        return {
+            verified_image: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pspnet-vue/src/views/UserProfile.vue
+++ pspnet-vue/src/views/UserProfile.vue
@@ -1,44 +1,50 @@
 <!-- Author: Antonio Lang -->
 
 <template>
+  <div class="col span-2 gap-4 p-2">
     <div>
-        <h1>"First Name" "Last Name"</h1>
-        <p>Created account on MM/DD/YYYY</p>
-        <p>Verified Labeler: "Yes/No"</p>
-        <p>No. of Contributions: "#""</p>
+        <h1>{{ first_name }} {{ last_name }}</h1>
+        <p>Created account on {{ create_date }}</p>
+        <p>Verified Labeler: {{ verified }}</p>
+        <p>No. of Contributions: {{ num_images }}</p>
     </div>
     <div class="user-bio">
-        <p>This information will be referenced from the user database.</p>
+        <p>{{ user_bio }}</p>
     </div>
     <div class="user-image-gallery">
-        <h2>Images Contributed</h2>
-            <p>These contributions will be referenced from the image database.</p>
-            <ul class="mdc-image-list my-image-list">
-                <li class="mdc-image-list__item">
-                  <div class="mdc-image-list__image-aspect-container">
-                    <img class="mdc-image-list__image" src="../assets/sage.jpg">
-                  </div>
-                  <div class="mdc-image-list__supporting">
-                    <span class="mdc-image-list__label">"Un/verified"</span>
-                  </div>
-                </li>
-                <li class="mdc-image-list__item">
-                    <div class="mdc-image-list__image-aspect-container">
-                      <img class="mdc-image-list__image" src="../assets/sage.jpg">
-                    </div>
-                    <div class="mdc-image-list__supporting">
-                      <span class="mdc-image-list__label">"Un/verified"</span>
-                    </div>
-                  </li>
-              </ul>
+      <h2>Images Contributed</h2>
+          <ul class="mdc-image-list my-image-list">
+            <li class="mdc-image-list__item">
+              <UserImg src_path="sage.jpg"/>
+            </li>
+          </ul>
     </div>
+  </div>
     <!-- Reference user's name, account creation data, verifier, bio from database -->
     <!-- count number of contributions from image database -->
     <!-- Display images from the image database with label -->
 </template>
 
 <script>
+import UserImg from '../components/UserImg.vue'
 
+export default {
+  data() {
+    return {
+      first_name: "John",
+      last_name: "Doe",
+      create_date: '01/01/1970',
+      user_bio: 'Hello! I like studying the effects of wildfire!',
+      num_images: 1,
+      img_paths: ['../assets/sage.jpg'],
+      verified: false
+    }
+  },
+  components: {UserImg},
+  setup() {
+
+  }
+}
 </script>
 
 <style>

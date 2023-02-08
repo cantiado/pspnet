7f278ab71077ae08f9e88a77ed3aa41bbb04cee1
Carl Antiado <carl.antiado01@gmail.com>
2023-02-07 22:22:57 -0800

    Implemented ImageCard with optional delete button and used it in Identify page image gallery

---
+++ pspnet-vue/src/components/ImageCard.vue
@@ -0,0 +1,36 @@
+<!-- Author: Carl Antiado -->
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg h-fit">
+    <img class="w-full aspect-square" :src="filePath">
+    <div class="px-3 py-2 flex justify-between items-center">
+      <div class="font-bold text-sm">{{ fileName.length > MAX_STR_LEN ? fileName.slice(0, MAX_STR_LEN - 3) + "..." : fileName }}</div>
+      <button v-if="!noDelete" @click="deleteImageCard" class="w-8"><XCircleIcon /></button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { XCircleIcon} from '@heroicons/vue/20/solid'
+
+export default {
+  emits: ["destroy"],
+  props: {
+    filePath: String,
+    fileName: String,
+    noDelete: Boolean
+  },
+  components: {
+    XCircleIcon
+  },
+  data() {
+    return {
+      MAX_STR_LEN: 20
+    }
+  },
+  methods: {
+    deleteImageCard() {
+      this.$emit("destroy");
+    }
+  }
+}
+</script>
--- pspnet-vue/src/views/IdentifyView.vue
+++ pspnet-vue/src/views/IdentifyView.vue
@@ -1,15 +1,16 @@
+<!-- Author: Carl Antiado -->
 <template>
   <div class="w-full flex flex-row flex-wrap">
-    <div class="basis-1/3 flex flex-col">
+    <div class="basis-1/3 flex flex-col p-5">
       <form method="POST" action="" enctype="multipart/form-data">
-        <div id="file-upload" class="flex items-center justify-center font-sans m-5">
+        <div id="file-upload" class="flex items-center justify-center font-sans">
           <label for="dropzone-file" class="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-blue-400 bg-white p-6 text-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
             </svg>
             <h2 class="mt-4 text-xl font-medium text-gray-700 tracking-wide">Choose Images</h2>
             <p class="mt-2 text-gray-500 tracking-wide">Upload or drag & drop your file PNG or JPEG. </p>
-            <input @change="uploadImages" id="dropzone-file" type="file" accept="image/png, image/jpeg" class="hidden" />
+            <input @change="uploadImages" id="dropzone-file" type="file" accept="image/png, image/jpeg" class="hidden" multiple />
           </label>
         </div>
         <div v-if="!uploadedImages" class="m-5 flex flex-col items-center">
@@ -20,7 +21,7 @@
           </button>
           <span>Please upload image(s)</span>
         </div>
-        <div v-else-if="selectedModel=='None'" class="m-5 flex flex-col items-center">
+        <div v-else-if="!selectedModel" class="m-5 flex flex-col items-center">
           <button disabled class="w-5/6 h-10 bg-gray-200 rounded-full">
             <span class="text-xl font-bold">
               Identify
@@ -41,7 +42,7 @@
         <div class="grid grid-cols-2 gap-4 text-right items-center">
           <span class="">Choose model:</span>
           <MenuButton class="inline-flex justify-end rounded-md bg-gray-200 px-4 py-2 font-medium hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-opacity-75">
-            {{ selectedModel }}
+            {{ selectedModel ? selectedModel : "Choose a model" }}
             <ChevronDownIcon
             class="ml-2 -mr-1 h-5 w-5"
             aria-hidden="true"
@@ -75,51 +76,66 @@
         </transition>
       </Menu>
     </div>
-    <div class="basis-2/3">
-      <div v-if="uploadedImages" v-for="file in dropzoneFile.files">
-        <span>{{ file.name }}</span>
-      </div>
-      <div v-else>
-        <span>No images uploaded</span>
+    <div v-if="uploadedImages" class="basis-2/3 grid grid-cols-3 gap-4 p-5">
+      <ImageCard v-for="file of files" @destroy="deleteFile(file)" :filePath="file.path" :fileName="file.name"/>
+    </div>
+    <div v-else class="basis-2/3 p-5">
+      <div class="h-full flex flex-col place-content-center rounded-xl border-2 border-dashed border-slate-500 bg-white">
+        <span class="text-xl text-slate-500">No images uploaded</span>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script>
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/20/solid'
-</script>
+import ImageCard from "../components/ImageCard.vue"
 
-<script>
 export default {
+  components: {
+    Menu, MenuButton, MenuItem, MenuItems, ChevronDownIcon, ImageCard
+  },
   data() {
     return {
       uploadedImages: false,
-      selectedModel: 'None',
+      selectedModel: '',
       models: [
         { name: 'Model 1' },
         { name: 'Model 2' },
         { name: 'Model 3' },
         { name: 'Model 4' },
       ],
-      dropzoneFile: null
+      dropzoneFile: null,
+      files: []
     }
   },
   mounted() {
-    this.dropzoneFile = document.getElementById('dropzone-file')
+    this.dropzoneFile = document.getElementById('dropzone-file');
   },
   methods: {
     setModel(model) {
       this.selectedModel = model.name;
     },
-    uploadImages() {
+    uploadImages(event) {
       if (this.dropzoneFile.files.length != 0) {
         this.uploadedImages = true;
+        for (let file of event.target.files) {
+          this.files.push({ name: file.name, path: URL.createObjectURL(file) })
+        }
       }
       else {
         this.uploadedImages = false;
       }
+    },
+    deleteFile(file) {
+      this.files = this.files.filter((f) => f !== file);
+      console.log(this.files)
+      console.log(this.dropzoneFile.files)
+      for (const f of this.dropzoneFile.files) {
+        console.log(f)
+      }
+      if (this.files.length == 0) this.uploadedImages = false;
     }
   }
 }

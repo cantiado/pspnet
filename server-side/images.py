9c8e441d3e95484e5dd5426bf66736810f8e4716
Carl Antiado <carl.antiado01@gmail.com>
2023-02-14 11:09:27 -0800

    Created flask app which receives images from front end and saves it to /images directory

---
+++ server-side/images.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+image_folder = "images"
+
+@app.route("/")
+def hello_world():
+  return "<h1>Hello world<h1>"
+
+@app.route("/identify", methods=["POST"])
+def identify():
+  print("here!")
+  files = request.files.to_dict(flat=False)["dropzone-file"]
+  for i, file in enumerate(files):
+    file.save(os.path.join(image_folder, file.filename))
+  return "success"

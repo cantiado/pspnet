507f56f3ea1f94ae88d3a739b49122b1be6da6b2
antoniolang1107 <TonylsLame@users.noreply.github.com>
2023-02-15 00:30:31 -0800

    Added Image table and successfully added test image from app.py

--- server-side/app.py
+++ server-side/app.py
@@ -10,6 +10,7 @@ import datetime
 
 
 app = Flask(__name__)
+app.app_context().push()
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///user.db"
 app.config['SECRET_KEY'] = '95fd1e474cbc4b49a3286dc09cba7510'
@@ -17,6 +18,7 @@ CORS(app, resources={r"/*":{'origins':"*"}})
 db = SQLAlchemy(app)
 
 db.init_app(app)
+db.create_all()
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +38,35 @@ class User(db.Model):
 #with app.app_context():
 #  db.session.add(testUser)
 #  db.session.commit()
+
+
+class Image(db.Model):
+  image_id = db.Column(db.String[20], primary_key=True)
+  path = db.Column(db.String[40], nullable=False)
+  uploader_id = db.Column(db.Integer, nullable=False)
+  label = db.Column(db.String[30], nullable=True)
+  access = db.Column(db.Integer, nullable=False)
+  location = db.Column(db.String[30], nullable=True)
+  verifier_id = db.Column(db.Integer, nullable=True)
+  upload_id = db.Column(db.Integer, nullable=False)
+  dataset_name = db.Column(db.String[20], nullable=True)
+
+  def __init__(self, image_id, path, uploader_id, upload_id, dataset_name,
+               verifier_id=None, label=None, location=None, access=0):
+    self.image_id = image_id
+    self.path = path
+    self.uploader_id = uploader_id
+    self.label = label
+    self.access = access
+    self.location = location
+    self.verifier_id = verifier_id
+    self.upload_id = upload_id
+    self.dataset_name = dataset_name
+  # insert into image values ("test-image-id", "/src/assets/user_images/sage.jpg",0,NULL,0,NULL, NULL, 0, "test")
+  # test_image = Image('test-image-id4', '/src/assets/user_images/sage.jpg', upload_id=0, uploader_id=0,
+  #     dataset_name="test", verifier_id=None, label=None,location=None,access=0)
+  # db.session.add(test_image)
+  # db.session.commit()
 
 #creates wrapper function for routes that require authorized tokens. 
 #code adapted from blog post https://stackabuse.com/single-page-apps-with-vue-js-and-flask-jwt-authentication/
@@ -109,6 +140,12 @@ def userdata(user):
     'name' : user.firstname + " " + user.lastname,
     'email' : user.email,
   }, 201
+
+@app.route('/profile/', methods = ['GET'])
+@token_required
+def profile():
+  all_img = Image.query.get("image_id")
+  return jsonify(all_img), 201
 
 if __name__ == "__main__":
   app.run(debug=True)

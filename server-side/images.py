23e6deef856824b258a8476ffd83d3ac3faa4f1d
antoniolang1107 <TonylsLame@users.noreply.github.com>
2023-03-06 12:30:11 -0800

    Images uploads are added to a folder based on their upload/job ID Classification is done on the specific folder and not all of the images Placeholders added to accept incoming dataset and user information on request

--- server-side/images.py
+++ server-side/images.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from app import Image
+from app import Dataset
+from app import Upload
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///user.db"
@@ -18,9 +20,21 @@ image_folder = "images"
 @app.route("/identify", methods=["POST"])
 def identify():
   files = request.files.to_dict(flat=False)["image-input"]
+  user_id = 0
+  dataset_name = "test"
+  # dataset_description = None
+  # new_dataset = Dataset(dataset_name, dataset_description)
+  # db.session.add(new_dataset)
+  new_upload = Upload(0)
+  db.session.add(new_upload)
+  db.session.commit()
+  job_id = db.session.query(Upload.id).order_by(Upload.id.desc()).first()[0]
+  
   for i, file in enumerate(files):
-    file.save(os.path.join(image_folder, file.filename))
-    new_image = Image(os.path.join(image_folder, file.filename), 0, 0, "test")
+    job_folder = os.path.join(image_folder, str(job_id)) 
+    os.makedirs(job_folder)
+    file.save(os.path.join(job_folder, file.filename))
+    new_image = Image(os.path.join(job_folder, file.filename), user_id, job_id, dataset_name)
     db.session.add(new_image)
     db.session.commit()
     # save file paths to image database
@@ -30,7 +44,7 @@ def identify():
   path = FILE.parents[0]
   os.chdir(path)
   print("Predicting...")
-  cmd = r'python yolov5/classify/predict.py --weights yolov5/best.onnx --save-txt --source images/ --img 640'
+  cmd = r'python yolov5/classify/predict.py --weights yolov5/best.onnx --save-txt --source images/' + str(job_id) + r' --img 640'
   os.system(cmd)
 
   #Declare string variables, append with new information from inference txt, print out
@@ -47,7 +61,7 @@ def identify():
   os.chdir(path)
   read_file = pd.read_csv (r'labels/predictions.txt')
   read_file.to_csv (r'labels/predictions.csv', index=None)
-  
+
   return open(r"labels/predictions.csv", mode='r')
 #2/27/2023 - Convert text files in predict class to csv...
 

ee74ed3848d651c9b4b7c671ce85e35ea16141e4
Carl Antiado <carl.antiado01@gmail.com>
2023-03-28 16:25:30 -0700

    Refactored code to be of Compositional API

--- pspnet-vue/src/views/LoginView.vue
+++ pspnet-vue/src/views/LoginView.vue
@@ -4,35 +4,93 @@
       <h1 class="text-4xl text-center">Login to Your Account</h1>
       <form @submit.prevent="handleLogin" class="form">
         <label class="label">Email:</label>
-        <input ref="emailInput" v-model="email" class="input" type="email" required>
+        <input
+          ref="emailInput"
+          v-model="email"
+          class="input"
+          type="email"
+          required
+        />
 
         <label class="label">Password:</label>
-        <input v-model="pass" class="input" type="password" required>
+        <input v-model="pass" class="input" type="password" required />
         <div class="flex justify-center">
-          <router-link class="underline text-blue-500" :to="{name: 'forgot'}">Forgot Password?</router-link>
+          <router-link class="underline text-blue-500" :to="{ name: 'forgot' }"
+            >Forgot Password?</router-link
+          >
         </div>
 
-        <div v-if="error_msg" class="error"> {{error_msg}}</div>
+        <div v-if="error_msg" class="error">{{ error_msg }}</div>
 
         <div class="flex justify-center">
-          <button v-if="!store.loading_data" class="bg-soil text-white hover:bg-pecan button">Sign In</button>
-          <button v-else class="bg-soil text-white button disabled:opacity-50 disabled ">Loading...</button>
+          <button
+            v-if="!store.loading_data"
+            class="bg-soil text-white hover:bg-pecan button"
+          >
+            Sign In
+          </button>
+          <button
+            v-else
+            class="bg-soil text-white button disabled:opacity-50 disabled"
+          >
+            Loading...
+          </button>
         </div>
       </form>
     </div>
     <div class="bg-soil col-span-4 grid place-content-center">
       <div class="max-w-md">
         <h2 class="text-4xl text-white my-6 text-center">New User?</h2>
-        <p class="text-white my-6 text-center">Create an account to upload and contribute to the community!</p>
+        <p class="text-white my-6 text-center">
+          Create an account to upload and contribute to the community!
+        </p>
         <div class="flex justify-center">
-          <button @click="handleCreate" class="bg-plant  text-black button hover:bg-green-400">Create Account</button>
+          <button
+            @click="handleCreate"
+            class="bg-plant text-black button hover:bg-green-400"
+          >
+            Create Account
+          </button>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import { authStore } from "@/store/authenticate";
+
+const pass = ref("");
+const email = ref("");
+const router = useRouter();
+
+const emailInput = ref(null);
+
+const store = authStore();
+const error_msg = ref("");
+
+onMounted(() => {
+  emailInput.value.focus();
+});
+
+async function handleLogin() {
+  error_msg.value = await store.loginUser({
+    email: email.value,
+    password: pass.value,
+  });
+  if (!error_msg.value) {
+    router.push({ name: "home" });
+  }
+}
+
+function handleCreate() {
+  router.push({ name: "register" });
+}
+</script>
+
+<!-- <script>
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { authStore } from '@/store/authenticate'
@@ -68,7 +126,6 @@ export default {
     return {pass, email, handleLogin, handleCreate, error_msg, store, emailInput}
   }
 }
-</script>
+</script> -->
 
-<style lang="postcss">
-</style>
+<style lang="postcss"></style>

aeabbfa024fde17d1219dbebe9983a4e804a8170
Carl Antiado <carl.antiado01@gmail.com>
2023-04-04 11:01:47 -0700

    Restyled to look nicer

--- pspnet-vue/src/components/DatasetView.vue
+++ pspnet-vue/src/components/DatasetView.vue
@@ -1,45 +1,46 @@
 <template>
-    <div class="w-full h-[79vh] flex flex-row">
-        <div class="basis-1/3 p-5">
-            <router-link to="/explore">
-                <div class="rounded p-4 bg-[#b9e0a5] text-white">
-                    Back to Explore
-                </div>
-            </router-link>
-            <div>
-                <ul>
-                    <li><text class="font-bold">{{ dsName }}</text></li>
-                    <li>No. of Images: {{ numImages }}</li>
-                    <li>No. of Uploads: {{ numUploads }}</li>
-                    <!-- <li># Contributors: {{ numContributers }}</li> -->
-                    <!-- <li># Modifications: </li> -->
-                    <li>Size of Dataset: {{ dsSize }} MB</li>
-
-                </ul>
-            </div>
-        </div>
-        <div class="basis-2/3 p-6 max-h-[79.5vh] overflow-auto">
-            <div class="imgContainer inline-grid grid-cols-1 gap-3">
-              <div v-for="(value, index) in imgData">
-                <div>
-                  <div class="text-xl font-bold">Upload: {{ index+1 }}</div>
-                  <div>Number of Images: {{ value['count'] }}</div>
-                  <div>Submitted By: {{ value['user'] }}</div>
-                  <div class="inline-grid grid-cols-3 gap-3" v-for="(img_value, img_index) in value['images']">
-                    <div>
-                      <img
-                            class="object-cover h-48 w-48 p-1 bg-white border rounded max-w-sm"
-                            :src="img_value"
-                        />
-                        <span>{{ value['labels'][img_index] }}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </div>
+  <div class="w-full h-[79vh] flex flex-row">
+    <div class="basis-1/4 p-5 bg-slate-50">
+      <div class="flex flex-row items-center">
+        <router-link to="/explore" class="basis-1/3">
+          <div class="rounded px-3 py-2 bg-[#b9e0a5] text-white">
+            Back to Explore
+          </div>
+        </router-link>
+        <h1 class="basis-2/3 font-bold text-xl">{{ dsName }}</h1>
+      </div>
+      <div class="mt-5 border-t-2 pt-5 text-left">
+        <h2 class="font-bold text-lg">Information</h2>
+        <ul>
+          <li>No. of Images: {{ numImages }}</li>
+          <li>No. of Uploads: {{ numUploads }}</li>
+          <!-- <li># Contributors: {{ numContributers }}</li> -->
+          <!-- <li># Modifications: </li> -->
+          <li>Size of Dataset: {{ dsSize }} MB</li>
+        </ul>
+      </div>
     </div>
-
+    <div class="basis-3/4 p-5 max-h-[79.5vh] overflow-auto">
+      <div class="grid grid-cols-1 gap-3">
+        <div v-for="(value, index) in imgData" class="p-5 flex flex-col gap-5 border-2 rounded-lg">
+          <div class="flex flex-row items-center gap-5">
+            <h2 class="text-xl font-bold">Upload: {{ index + 1 }}</h2>
+            <span>Number of Images: {{ value["count"] }}</span>
+            <span>Submitted By: {{ value["user"] }}</span>
+          </div>
+          <div class="flex flex-row flex-wrap gap-5">
+            <div v-for="(img_value, img_index) in value['images']">
+              <img
+                class="object-cover h-48 w-48 p-1 bg-white border rounded max-w-sm"
+                :src="img_value"
+              />
+              <span>{{ value["labels"][img_index] }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -52,7 +53,7 @@ import b64toBlob from "@/composables/byteToBlob";
 export default {
   props: {
     dsName: String,
-    numImages: Number
+    numImages: Number,
   },
 
   setup(props) {
@@ -66,18 +67,18 @@ export default {
     const dsSize = ref(0);
 
     const getURLs = (imgBytes) => {
-      var returnURLs = []
+      var returnURLs = [];
       for (var i = 0; i < imgBytes.length; i++) {
         returnURLs.push(URL.createObjectURL(b64toBlob(imgBytes[i])));
       }
-      return returnURLs
+      return returnURLs;
     };
 
     const convertByUpload = (imageData) => {
-        for (var i = 0; i < imageData.length; i++) {
-          imgData.value[i]["images"] = getURLs(imageData[i]["images"]);
-        };
-      };
+      for (var i = 0; i < imageData.length; i++) {
+        imgData.value[i]["images"] = getURLs(imageData[i]["images"]);
+      }
+    };
 
     onMounted(async () => {
       if (dsName.value) {
@@ -85,14 +86,14 @@ export default {
           .get("http://127.0.0.1:5000/datasetview/" + dsName.value + "/")
           .then(
             (response) => (
-                (responseData.value = response.data),
-                imgData.value = response.data["upload_data"],
-                convertByUpload(response.data["upload_data"]),
-                numUploads.value = response.data["upload_data"].length,
-                numImages.value = response.data["num_images"],
-                dsSize.value = response.data["ds_size"],
-                console.log(responseData.value),
-                console.log(imgData.value)
+              (responseData.value = response.data),
+              (imgData.value = response.data["upload_data"]),
+              convertByUpload(response.data["upload_data"]),
+              (numUploads.value = response.data["upload_data"].length),
+              (numImages.value = response.data["num_images"]),
+              (dsSize.value = response.data["ds_size"]),
+              console.log(responseData.value),
+              console.log(imgData.value)
             )
           )
           .catch((error.value = "Failed to retreive data"));

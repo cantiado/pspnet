dee63f72fb40d77f8c10b1f6b681405ddb2f126e
Carl Antiado <carl.antiado01@gmail.com>
2023-04-13 15:13:31 -0700

    Changed models to model

--- pspnet-vue/src/components/DatasetSection.vue
+++ pspnet-vue/src/components/DatasetSection.vue
@@ -1,8 +1,10 @@
 <template>
   <h1 class="text-3xl mb-5">Dataset</h1>
   <p>
-    The current dataset used to train our models is the Pl@ntNet-300K data set.
-    In the future, we are hoping to receive the specific data set to the
+    The current dataset used to train our model is the
+    <a class="text-blue-500 underline" href="https://plantnet.org/open-data/"
+      >Pl@ntNet-300K data set</a
+    >. In the future, we are hoping to receive the specific data set to the
     wildfire research project. However, gathering the data is a labor and time
     insensive project and likely will not be completed in time to train on.
   </p>

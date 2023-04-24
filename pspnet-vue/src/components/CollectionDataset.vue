ccc7320ae63c85ff77c46e496bee09c49d0ebd9a
Carl Antiado <carl.antiado01@gmail.com>
2023-04-23 18:28:35 -0700

    Added emit to inform when clicked

--- pspnet-vue/src/components/CollectionDataset.vue
+++ pspnet-vue/src/components/CollectionDataset.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="flex flex-col p-3 gap-2 bg-slate-100 rounded-lg">
-    <div class="h-fit flex flex-row items-center gap-2">
+  <button
+    class="flex flex-col p-3 gap-2 bg-slate-100 hover:bg-slate-300 focus:bg-slate-300 rounded-lg"
+    @click="emit('clicked', datasetName)"
+  >
+    <div class="h-fit w-full flex flex-row items-center gap-2">
+      <PhotoIcon class="w-6 h-6" />
       <span class="text-left truncate">{{ datasetName }}</span>
     </div>
     <img v-if="imageSrc" :src="imageSrc" class="aspect-square min-w-24" />
     <div
       v-else
-      class="text-lg flex flex-row justify-center items-center border-2 border-dashed border-slate-300 min-w-24 aspect-square"
+      class="text-lg w-full flex flex-row justify-center items-center border-2 border-dashed border-slate-500 min-w-24 aspect-square"
     >
       No image
     </div>
-  </div>
+  </button>
 </template>
 
 <script setup>
@@ -20,6 +24,8 @@ const props = defineProps({
   datasetName: String,
   imageSrc: String,
 });
+
+const emit = defineEmits(["clicked"])
 </script>
 
 <!-- <script>

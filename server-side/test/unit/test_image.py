8e331467ef3aa5ea629c6179ab7ec43c618f667a
antoniolang1107 <TonylsLame@users.noreply.github.com>
2023-04-24 16:14:25 -0700

    Updated tests to reflect database schema changes

--- server-side/test/unit/test_image.py
+++ server-side/test/unit/test_image.py
@@ -8,12 +8,11 @@ def test_new_image():
     WHEN a new Image is uploaded to the system
     THEN check if the image has the correct defined and default data
     """
-    image = Image(0, 'src/assets/user_images/sage.jpg', 0, 0, 'test_dataset')
+    image = Image('images/1/sage.jpg', 1, 1, 'test_dataset')
 
-    assert image.image_id == 0
-    assert image.path == 'src/assets/user_images/sage.jpg'
-    assert image.uploader_id == 0
-    assert image.upload_id == 0
+    assert image.path == 'images/1/sage.jpg'
+    assert image.uploader_id == 1
+    assert image.upload_id == 1
     assert image.dataset_name == 'test_dataset'
     assert image.verifier_id == None
     assert image.label == None

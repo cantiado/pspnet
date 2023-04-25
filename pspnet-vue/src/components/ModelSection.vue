1c68164f8b651085ce5ffb617e3d94c15ee39588
Carl Antiado <carl.antiado01@gmail.com>
2023-04-25 14:55:05 -0700

    Fixed code line boxes

--- pspnet-vue/src/components/ModelSection.vue
+++ pspnet-vue/src/components/ModelSection.vue
@@ -1,30 +1,40 @@
 <template>
-  <h1 class="text-3xl mb-5">Machine Learning Model: <a class="text-blue-500 underline" href="https://docs.ultralytics.com/yolov5/"
+  <h1 class="text-3xl mb-5">
+    Machine Learning Model:
+    <a
+      class="text-blue-500 underline"
+      href="https://docs.ultralytics.com/yolov5/"
       >YOLOv5</a
-    ></h1>
-  <h2>
-    <b>How to setup and install YOLOv5 to run PSPNet</b>
+    >
+  </h1>
+  <h2 class="text-lg font-bold">
+    How to setup and install YOLOv5 to run PSPNet
   </h2>
-  <p>
-    Clone repo and install requirements.txt in a Python environment, 
-    including PyTorch. Models and datasets
-    download automatically from the latest YOLOv5 release with the following commands:
+  <p class="mb-5">
+    Clone repo and install requirements.txt in a Python environment, including
+    PyTorch. Models and datasets download automatically from the latest YOLOv5
+    release with the following commands:
   </p>
-  <p style="border-width:4px; border-style:solid; border-color:#000000; padding: 1em;">
-  git clone https://github.com/ultralytics/yolov5      #clone
-  <br>cd yolov5
-  <br>pip install -r requirements.txt  #install
+  <div class="flex flex-col mb-5 bg-gray-100 px-2 py-1 rounded-md">
+    <code class="truncate"
+      >git clone https://github.com/ultralytics/yolov5 #clone</code
+    >
+    <code>cd yolov5</code>
+    <code>pip install -r requirements.txt #install</code>
+  </div>
+  <p class="mb-5">
+    Now, download our trained weight:
+    <a
+      class="text-blue-500 underline"
+      href="https://drive.google.com/file/d/1GzaauTlnI9veRKHsFRiC1zMqqwWtCofC/view?usp=sharing"
+      >PSPNet Trained Weight.</a
+    >
   </p>
-  <p>
-    <br>Now, download our trained weight: <a class="text-blue-500 underline" href="https://drive.google.com/file/d/1GzaauTlnI9veRKHsFRiC1zMqqwWtCofC/view?usp=sharing"
-      >PSPNet Trained Weight.</a><br>
+  <p class="mb-5">
+    To run a prediction, execute the following commands:
   </p>
-  <p>
-    <br>To run a prediction, execute the following commands:
-    <p style="border-width:4px; border-style:solid; border-color:#000000; padding: 1em;">
-      python classify/predict.py --weights best.onnx --img 640 --source "path to image file or directory of images" --save-txt
-
-  </p>
-
-  </p>
+    <code class="mb-5 bg-gray-100 px-2 py-1 rounded-md"
+      >python classify/predict.py --weights best.onnx --img 640 --source "path
+      to image file or directory of images" --save-txt
+    </code>
 </template>

87b6cf3d4b9f53d286cf8f8d71343466577f1f53
Carl Antiado <carl.antiado01@gmail.com>
2023-05-04 11:06:01 -0700

    Added loading spinner

--- pspnet-vue/src/components/DatasetView.vue
+++ pspnet-vue/src/components/DatasetView.vue
@@ -2,7 +2,11 @@
 
 <template>
   <div v-if="showUploadModal">
-      <UploadPopup :dsName="dsName" :uploadID="modalUploadID" @closeModal="closeModal"/>
+    <UploadPopup
+      :dsName="dsName"
+      :uploadID="modalUploadID"
+      @closeModal="closeModal"
+    />
   </div>
   <div class="w-full h-[79vh] flex flex-row">
     <div class="basis-1/4 p-5 bg-slate-50">
@@ -23,15 +27,20 @@
           <!-- <li># Modifications: </li> -->
           <li>Size of Dataset: {{ dsSize }} MB</li>
           <li>
-            <button class="rounded px-3 py-2 bg-[#b9e0a5] text-white m-1"
+            <button
+              class="rounded px-3 py-2 bg-[#b9e0a5] text-white m-1"
               :href="url"
-              @click.prevent="downloadDataset(dsName)">
+              @click.prevent="downloadDataset(dsName)"
+            >
               Download
             </button>
           </li>
           <li>
-            <button v-if="userID > 0" class="rounded px-3 py-2 bg-[#b9e0a5] text-white m-1"
-              @click.prevent="saveDataset()">
+            <button
+              v-if="userID > 0"
+              class="rounded px-3 py-2 bg-[#b9e0a5] text-white m-1"
+              @click.prevent="saveDataset()"
+            >
               Save Dataset
             </button>
           </li>
@@ -39,33 +48,72 @@
       </div>
     </div>
     <div class="basis-3/4 p-5 max-h-[79.5vh] overflow-auto">
-      <div class="grid grid-cols-1 gap-3">
-        <div v-for="(value, index) in imgData.slice().reverse()"
-          class="p-5 flex flex-col gap-5 border-2 rounded-lg">
+      <div v-if="imgData.length == 0 && !responseData" class="w-full h-full flex flex-row items-center justify-center" role="status">
+        <svg
+          aria-hidden="true"
+          class="w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div v-else class="grid grid-cols-1 gap-3">
+        <div
+          v-for="(value, index) in imgData.slice().reverse()"
+          class="p-5 flex flex-col gap-5 border-2 rounded-lg"
+        >
           <div class="flex flex-row items-center gap-5">
-            <button class="text-xl font-bold border border-1 rounded p-1"
-            @click="openModal(value['id'])">
-            Upload ID: {{ value['id'] }}</button>
+            <button
+              class="text-xl font-bold border border-1 rounded p-1"
+              @click="openModal(value['id'])"
+            >
+              Upload ID: {{ value["id"] }}
+            </button>
             <span>Number of Images: {{ value["count"] }}</span>
             <span>Submitted By: {{ value["user"] }}</span>
-            <span>Date: {{ value['timestamp'] }}</span>
+            <span>Date: {{ value["timestamp"] }}</span>
             <span v-if="value['notes']">Notes: {{ value["notes"] }}</span>
-            
-            <button 
-              v-if="role==='Principal Investigator' && !value['verified']" 
-              @click="value['verified'] = updateLabels(value['verified'],value['id'],index)"
-              class="border rounded p-1 border-black">
+
+            <button
+              v-if="role === 'Principal Investigator' && !value['verified']"
+              @click="
+                value['verified'] = updateLabels(
+                  value['verified'],
+                  value['id'],
+                  index
+                )
+              "
+              class="border rounded p-1 border-black"
+            >
               Verify Labels
             </button>
-            <button 
-              v-else-if="role==='Principal Investigator' && value['verified']" 
-              @click="value['verified'] = updateLabels(value['verified'],value['id'],index)"
-              class="italic border rounded p-1 border-[#b9e0a5]">
+            <button
+              v-else-if="role === 'Principal Investigator' && value['verified']"
+              @click="
+                value['verified'] = updateLabels(
+                  value['verified'],
+                  value['id'],
+                  index
+                )
+              "
+              class="italic border rounded p-1 border-[#b9e0a5]"
+            >
               Labels Verified
             </button>
-            <div 
-              v-else-if="value['verified']" 
-              class="italic border rounded p-1 border-[#b9e0a5]">
+            <div
+              v-else-if="value['verified']"
+              class="italic border rounded p-1 border-[#b9e0a5]"
+            >
               Labels Verified
             </div>
             <!-- check conditions for the button vs not -->
@@ -92,7 +140,7 @@ import UserImg from "./UserImg.vue";
 import UploadPopup from "./UploadPopup.vue";
 import { onMounted } from "vue";
 import b64toBlob from "@/composables/byteToBlob";
-import { authStore } from '@/store/authenticate'
+import { authStore } from "@/store/authenticate";
 
 export default {
   props: {
@@ -109,49 +157,53 @@ export default {
     const numUploads = ref(0);
     const numContributers = ref(0);
     const dsSize = ref(0);
-    const store = authStore()
+    const store = authStore();
     const role = ref(0);
     const userID = ref(0);
-    const baseDownloadURL = ref("http://127.0.0.1:5000/datasetview/")
-    const saveURL = ref("http://127.0.0.1:5000/explore/"+dsName.value+"/save/")
-    const showUploadModal = ref(false)
-    const modalUploadID = ref()
-
+    const baseDownloadURL = ref("http://127.0.0.1:5000/datasetview/");
+    const saveURL = ref(
+      "http://127.0.0.1:5000/explore/" + dsName.value + "/save/"
+    );
+    const showUploadModal = ref(false);
+    const modalUploadID = ref();
 
     const downloadDataset = (dsName) => {
-      var downloadURL = baseDownloadURL.value.concat(dsName).concat("/download/")
-      axios.get(downloadURL, { responseType: 'blob' })
-      .then(response => {
-        const blob = new Blob([response.data], {type: 'zip'})
-        const link = document.createElement('a')
-        link.href = URL.createObjectURL(blob)
-        link.download = dsName + ".zip"
-        link.click()
-        URL.revokeObjectURL(link.href)
-      }).catch(console.error)
-    }
+      var downloadURL = baseDownloadURL.value
+        .concat(dsName)
+        .concat("/download/");
+      axios
+        .get(downloadURL, { responseType: "blob" })
+        .then((response) => {
+          const blob = new Blob([response.data], { type: "zip" });
+          const link = document.createElement("a");
+          link.href = URL.createObjectURL(blob);
+          link.download = dsName + ".zip";
+          link.click();
+          URL.revokeObjectURL(link.href);
+        })
+        .catch(console.error);
+    };
 
     const saveDataset = () => {
       var success = false;
-      axios.post(saveURL.value, {id: userID.value})
-      .then(
-        (response) => {
-          console.log(response.data)
-          success = response.data['success']
-        }
-      )
-      .catch(console.log(""))
-      return success
-    }
+      axios
+        .post(saveURL.value, { id: userID.value })
+        .then((response) => {
+          console.log(response.data);
+          success = response.data["success"];
+        })
+        .catch(console.log(""));
+      return success;
+    };
 
     function openModal(uploadID) {
-      modalUploadID.value = uploadID
+      modalUploadID.value = uploadID;
       showUploadModal.value = true;
-    };
+    }
 
     function closeModal(close) {
       showUploadModal.value = false;
-    };
+    }
 
     const getURLs = (imgBytes) => {
       var returnURLs = [];
@@ -162,24 +214,30 @@ export default {
     };
 
     const checkRole = () => {
-      console.log(store)
-    }
+      console.log(store);
+    };
 
     const updateLabels = async (verified, uploadID, index) => {
-      var baseURL = "http://127.0.0.1:5000/datasetview/"
-      var extension = dsName.value.concat("/").concat(uploadID).concat('/').concat("updateLabel").concat('/')
-      var URL = baseURL.concat(extension)
-      var newLabel
+      var baseURL = "http://127.0.0.1:5000/datasetview/";
+      var extension = dsName.value
+        .concat("/")
+        .concat(uploadID)
+        .concat("/")
+        .concat("updateLabel")
+        .concat("/");
+      var URL = baseURL.concat(extension);
+      var newLabel;
       await axios.get(URL).then(
         (response) => (
-          newLabel = response.data['verified'],
-          imgData.value[imgData.value.length -1 - index]['verified'] = newLabel
+          (newLabel = response.data["verified"]),
+          (imgData.value[imgData.value.length - 1 - index]["verified"] =
+            newLabel)
           // verified.value = newLabel)
           // imgData.value[dsName][index]['verified'] = newLabel)
-          )
-          )
-        console.log(imgData.value)
-      return newLabel
+        )
+      );
+      console.log(imgData.value);
+      return newLabel;
     };
 
     const convertByUpload = (imageData) => {
@@ -187,12 +245,30 @@ export default {
         imgData.value[i]["images"] = getURLs(imageData[i]["images"]);
       }
     };
-    
+
     const convertToDate = (upload) => {
-      const monthNames = ["January", "February", "March", "April","May","June","July","August","September","October","November","December"]
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
       for (var i = 0; i < upload.length; i++) {
-        var timestamp = new Date(Number(upload[i]['timestamp']))
-        upload[i]['timestamp'] = String(timestamp.getFullYear()) + " " + monthNames[timestamp.getMonth()] + " " + String(timestamp.getDate())
+        var timestamp = new Date(Number(upload[i]["timestamp"]));
+        upload[i]["timestamp"] =
+          String(timestamp.getFullYear()) +
+          " " +
+          monthNames[timestamp.getMonth()] +
+          " " +
+          String(timestamp.getDate());
       }
     };
 
@@ -220,10 +296,23 @@ export default {
       }
     });
 
-    return { imgData, numImages, numUploads, numContributers, 
-      dsSize, updateLabels, role, url : baseDownloadURL, 
-      downloadDataset, saveDataset, userID,
-      openModal, closeModal, showUploadModal, modalUploadID };
+    return {
+      imgData,
+      numImages,
+      numUploads,
+      numContributers,
+      dsSize,
+      updateLabels,
+      role,
+      url: baseDownloadURL,
+      downloadDataset,
+      saveDataset,
+      userID,
+      openModal,
+      closeModal,
+      showUploadModal,
+      modalUploadID,
+    };
   },
   components: { UserImg, UploadPopup },
 };
